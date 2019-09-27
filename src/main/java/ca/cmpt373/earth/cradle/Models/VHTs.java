d8af46719ee0371d8463920ab61db8bfb9e7b331
Brian Marwood <bmarwood@sfu.ca>
2019-09-26 22:10:08 -0700

    Add Models, Controllers, and Repos for the backend, add mongodb collections respectively, confirmed passthrough from database to localhost

---
+++ src/main/java/ca/cmpt373/earth/cradle/Models/VHTs.java
@@ -0,0 +1,53 @@
+package ca.cmpt373.earth.cradle.Models;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "vhts")
+public class VHTs {
+
+    @Id
+    private String id; //primary key
+    private String name;
+    private String list_of_patients;
+    private String list_of_assessments;
+
+    public VHTs(String id, String name, String list_of_patients, String list_of_assessments) {
+        this.id = id;
+        this.name = name;
+        this.list_of_patients = list_of_patients;
+        this.list_of_assessments = list_of_assessments;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getList_of_patients() {
+        return list_of_patients;
+    }
+
+    public void setList_of_patients(String list_of_patients) {
+        this.list_of_patients = list_of_patients;
+    }
+
+    public String getList_of_assessments() {
+        return list_of_assessments;
+    }
+
+    public void setList_of_assessments(String list_of_assessments) {
+        this.list_of_assessments = list_of_assessments;
+    }
+}
+
--- src/main/java/ca/cmpt373/earth/cradle/config/MongoDBConfig.java
+++ src/main/java/ca/cmpt373/earth/cradle/config/MongoDBConfig.java
@@ -1,7 +1,13 @@
 package ca.cmpt373.earth.cradle.config;
 
+import ca.cmpt373.earth.cradle.Models.CHOs;
+import ca.cmpt373.earth.cradle.Models.Patients;
 import ca.cmpt373.earth.cradle.Models.Users;
+import ca.cmpt373.earth.cradle.Models.VHTs;
+import ca.cmpt373.earth.cradle.repository.CHOsRepository;
+import ca.cmpt373.earth.cradle.repository.PatientsRepository;
 import ca.cmpt373.earth.cradle.repository.UsersRepository;
+import ca.cmpt373.earth.cradle.repository.VHTsRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +26,37 @@ public class MongoDBConfig {
 
             usersRepository.save(new Users("4", "Peter", 4500.00, "username", hashedPassword));
             usersRepository.save(new Users("5", "Sam", 7500.00, "usernameB", hashedPassword));
+
+        };
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunnerPatients(PatientsRepository patientsRepository) {
+        return strings -> {
+            String hashedPassword = passwordEncoder.encode("password");
+
+            patientsRepository.save(new Patients("36", "Peter", "feb4 1996", "id of assessment 1, id of assessment2", "female"));
+            patientsRepository.save(new Patients("5", "Sam", "dec1 1960", "null", "male"));
+
+        };
+    }
+    @Bean
+    CommandLineRunner commandLineRunnerVHTs(VHTsRepository vhtsRepository) {
+        return strings -> {
+            String hashedPassword = passwordEncoder.encode("password");
+
+            vhtsRepository.save(new VHTs("115", "Bilbo Baggins", "id of patient 1", "id of assessment 1, id of assessment2"));
+            vhtsRepository.save(new VHTs("54", "Gregor the Overlander", "id of patient 1, id of patient 2", "id of assessment 1, id of assessment2"));
+
+        };
+    }
+    @Bean
+    CommandLineRunner commandLineRunnerCHOs(CHOsRepository chosRepository) {
+        return strings -> {
+            String hashedPassword = passwordEncoder.encode("password");
+
+            chosRepository.save(new CHOs("1105", "Bilbo Baggins", "id of vht 1,id of vht 2","id of patient 1", "id of assessment 1, id of assessment2"));
+            chosRepository.save(new CHOs("540", "Gregor the Overlander", "","id of patient 1, id of patient 2", "id of assessment 1, id of assessment2"));
 
         };
     }
