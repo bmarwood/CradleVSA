70e94904cc12e8e70c2b04cbee3f8d8a6c9ce88c
trieucamtoan <ctrieu@sfu.ca>
2019-09-28 17:11:38 -0700

    Remove Hello Page

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import LandingPage from './landingpage';
-import Hello from './hello';
 import Assessments from './assessments';
 import Login  from './login';
 
 const Navigation = () => (
   <Switch>
     <Route exact path = "/" component = {LandingPage} />
-    <PrivateRoute path = "/users/hello" component = {Hello} />
     <Route exact path = "/user-dashboard" component = {LandingPage} />
     <Route exact path = "/admin-dashboard" component = {LandingPage} />
     <Route exact path = "/login" component = {Login} />
