7348e57573077cddae1329a92c814b01e4e47c65
Gaeun Lee <gaeunl@sfu.ca>
2019-10-17 13:20:58 -0700

    debug

--- backend/src/main/java/ca/cmpt373/earth/cradle/Controllers/AssessmentsController.java
+++ backend/src/main/java/ca/cmpt373/earth/cradle/Controllers/AssessmentsController.java
@@ -24,6 +24,10 @@ public class AssessmentsController {
         this.assessmentsRepository = assessmentsRepository;
     }
 
+
+    @GetMapping("/all")
+    @ResponseStatus(code = HttpStatus.OK)
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public List<Assessments> getAll() {
         List<Assessments> assessments = null;
         try {
