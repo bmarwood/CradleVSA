a7c25df40f55bb8c0236c55c4849f56524f6153e
corey <crsee@sfu.ca>
2019-10-21 11:57:10 -0700

    Make home screen heart image clickable

--- frontend/src/components/landingpage.js
+++ frontend/src/components/landingpage.js
@@ -5,15 +5,29 @@ import heart from '../heart1.svg';
 import '../App.css';
 
 class LandingPage extends Component {
+    changePage () {
+        if (localStorage.getItem('isLoggedIn') === 'true'){
+            window.location.reload()
+        }
+        else{
+            window.location.assign("/login")
+        }
+    }
+
     render() {
         return (
             <div className="landing-form">
                 <h1 style={{color: "white"}}> Welcome to CRADLE</h1>
                 <p style={{color: "white"}}>Cradle VSA provides Blood pressure monitoring and Gestational Tracking</p>
-                <img src={heart} alt="logo"/>
+                <img src={heart} alt="logo" onClick={() => this.changePage()} />
+
             </div>
+
         );
+
     }
+
 }
+
 
 export default LandingPage;
