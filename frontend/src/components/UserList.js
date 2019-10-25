b9a26f8d187d0173ac9ac64acc1c46d4883435d8
fchandel <fchandel@sfu.ca>
2019-10-25 12:50:45 -0700

    Admin Landing Page now pulls from db

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -15,7 +15,7 @@ class UserList extends Component {
     }
 
     componentDidMount() {
-        this.getPatientList()
+        this.getUserList()
         this.setState({
             columns: [
                 { title: 'Username', field: 'username' },
@@ -29,16 +29,89 @@ class UserList extends Component {
             ],
             Data:[
                 {
-                    username: 'ahoward849567',
-                    password: 'password1',
-                    role: ["CHO "],
-                    name: 'Ann',
-                    surname: 'Howard',
-                    sex: 'Female',
-                    birthYear: 1987,
-                    id: 849567 }
+                    username: 'Loading',
+                    password: 'Loading',
+                    role: ["Loading "],
+                    name: 'Loading',
+                    surname: 'Loading',
+                    sex: 'Loading',
+                    birthYear: 'Loading',
+                    id: 'Loading' 
+                }
             ]
     
+        })
+    }
+
+    getUserRoles(user) {
+        var roleString = ''
+        if (user && user.roles) {
+            user.roles.forEach(role => {
+                roleString = roleString + " " + role.role
+            })
+            console.log('returning roles: ', roleString)
+            return roleString
+        }
+        console.log('returning empty roles: ')
+
+        return roleString
+    }
+
+    populateData(response) {
+        var UserList = []
+
+        response.forEach(user => {
+            var roleString = this.getUserRoles(user)
+  
+            var username = user.username
+            var password = user.password
+            var name = user.name
+            var surname = user.name.split(' ')[1]
+            var sex = user.gender
+            var birthYear = user.dob
+            var id = user.id
+
+            var user_obj = {
+                username: username,
+                password: password,
+                role: roleString,
+                name: name,
+                surname: surname,
+                sex: sex,
+                birthYear: birthYear,
+                id: id 
+            }
+
+            UserList.push(user_obj)
+        });
+
+        this.setState({ data: UserList })
+
+    }
+
+    async getUserList() {
+        var passback = await requestServer.getUserList()
+        if (passback !== null) {
+            this.populateData(passback.data)
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="table-position">
+                <MaterialTable
+                    title="User"
+                    columns={this.state.columns}
+                    data={this.state.data}
+                />
+            </div>
+
+        );
+    }
+
+
 }
 
-export default UserList
+
+export default UserList;
