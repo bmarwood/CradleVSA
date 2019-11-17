a896f4e8b5c018efbc77fe38f803bec6774e8f8a
Gaeun Lee <gaeunl@sfu.ca>
2019-11-17 12:56:19 -0800

    fix error

--- backend/src/main/java/ca/cmpt373/earth/cradle/Controllers/PatientsController.java
+++ backend/src/main/java/ca/cmpt373/earth/cradle/Controllers/PatientsController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
 
 import java.util.List;
 
