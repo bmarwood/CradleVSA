189bf6d5469fa232d58194034fe95c198ba9d820
crsee <corey_see@sfu.ca>
2019-11-20 01:28:44 -0800

    Create second table, and start some initial logic to change 1 select depending on the other

--- frontend/src/components/transferPatient.js
+++ frontend/src/components/transferPatient.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import requestServer from "./RequestServer";
 import MaterialTable from "material-table";
+import './Transfer.css';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -22,8 +23,11 @@ class TransferPatient extends Component {
             vht_array: [],
             vht_set: [],
             user_set: [],
-            vht_w_assessment: []
+            vht_w_assessment: [],
+            from_vht: '',
+            to_vht: ''
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -120,45 +124,45 @@ class TransferPatient extends Component {
         }
     }
 
-    async getAssessmentList() {
-        var userData = JSON.parse(localStorage.getItem("userData"))
-        var roles = this.getRoles(userData)
-        console.log("USER", userData)
-        var passback
-        if (this.isAdmin(roles)) {
-            passback = await requestServer.getAssessmentsList()
-
-            if (passback !== null && passback.data !== "") {
-                await this.vhtSet()
-                await this.userSet(passback.data)
-                let found = await this.parseThings(passback.data)
-                console.log("foound",found)
-
-
-
-                var tempdata = passback.data
-                console.log("PRE CHECK", passback.data)
-                for (let i in found)
-                {
-
-                    console.log("DELETING", passback.data[found[i]], "AT POSITION", found[i])
-                    delete passback.data[found[i]]
-                }
-
-                //delete passback.data[0]
-                console.log("FINAL CHECK", passback.data)
-                this.populateData(passback.data)
-                //this.setState( vht_select_option: interse)
-            }
-
-        } else {
-            passback = await requestServer.getAssessmentsByUserId(userData.id)
-
-            if (passback !== null && passback.data !== "") {
-                this.populateData(passback.data)
-            }
-        }
-    }
+    // async getAssessmentList() {
+    //     var userData = JSON.parse(localStorage.getItem("userData"))
+    //     var roles = this.getRoles(userData)
+    //     console.log("USER", userData)
+    //     var passback
+    //     if (this.isAdmin(roles)) {
+    //         passback = await requestServer.getAssessmentsList()
+    //
+    //         if (passback !== null && passback.data !== "") {
+    //             await this.vhtSet()
+    //             await this.userSet(passback.data)
+    //             let found = await this.parseThings(passback.data)
+    //             console.log("foound",found)
+    //
+    //
+    //
+    //             var tempdata = passback.data
+    //             console.log("PRE CHECK", passback.data)
+    //             for (let i in found)
+    //             {
+    //
+    //                 console.log("DELETING", passback.data[found[i]], "AT POSITION", found[i])
+    //                 delete passback.data[found[i]]
+    //             }
+    //
+    //             //delete passback.data[0]
+    //             console.log("FINAL CHECK", passback.data)
+    //             this.populateData(passback.data)
+    //             //this.setState( vht_select_option: interse)
+    //         }
+    //
+    //     } else {
+    //         passback = await requestServer.getAssessmentsByUserId(userData.id)
+    //
+    //         if (passback !== null && passback.data !== "") {
+    //             this.populateData(passback.data)
+    //         }
+    //     }
+    // }
 /*
     async getVHTListAll(){
         var passback = await requestServer.getVHTList()
@@ -167,29 +171,29 @@ class TransferPatient extends Component {
         }
     }
 */
-    async vhtSet(){
-        let vht_set1 = [];
-        console.log("IN VHT CHECK",this.state.vht_array)
-        for(let itr = 0; itr < Object.keys(this.state.vht_array).length; itr++){
-             vht_set1.push(this.state.vht_array[itr].id)
-         }
-        this.setState({
-            vht_set: vht_set1
-        })
-        console.log("IN VHT CHECK 1",this.state.vht_set)
-    }
-
-    async userSet(data){
-        let user_set1 = [];
-        //console.log("IN VHT CHECK",this.state.vht_array)
-        for(let itr = 0; itr < Object.keys(data).length; itr++){
-            user_set1.push(data[itr].cvsa_id)
-        }
-        this.setState({
-            user_set: user_set1
-        })
-        console.log("IN USER CHECK",this.state.user_set)
-    }
+    // async vhtSet(){
+    //     let vht_set1 = [];
+    //     console.log("IN VHT CHECK",this.state.vht_array)
+    //     for(let itr = 0; itr < Object.keys(this.state.vht_array).length; itr++){
+    //          vht_set1.push(this.state.vht_array[itr].id)
+    //      }
+    //     this.setState({
+    //         vht_set: vht_set1
+    //     })
+    //     console.log("IN VHT CHECK 1",this.state.vht_set)
+    // }
+    //
+    // async userSet(data){
+    //     let user_set1 = [];
+    //     //console.log("IN VHT CHECK",this.state.vht_array)
+    //     for(let itr = 0; itr < Object.keys(data).length; itr++){
+    //         user_set1.push(data[itr].cvsa_id)
+    //     }
+    //     this.setState({
+    //         user_set: user_set1
+    //     })
+    //     console.log("IN USER CHECK",this.state.user_set)
+    // }
 
 
     async getVHTList() {
@@ -202,59 +206,82 @@ class TransferPatient extends Component {
         }
     }
 
-    async parseThings(data){
-        let intersect = this.state.vht_set.filter(value => this.state.user_set.includes(value));
-        console.log("intersect", intersect)
-        let found = []
-        for (let itr = 0; itr < Object.keys(data).length; itr++) {
+    // async parseThings(data){
+    //     let intersect = this.state.vht_set.filter(value => this.state.user_set.includes(value));
+    //     console.log("intersect", intersect)
+    //     let found = []
+    //     for (let itr = 0; itr < Object.keys(data).length; itr++) {
+    //
+    //         if (!intersect.includes(data[itr].cvsa_id)) {
+    //             found.push(itr)
+    //         }
+    //     }
+    //     console.log("in getAss", intersect)
+    //     this.setState({vht_w_assessment: intersect})
+    //     console.log("in getAss with assessment", this.state.vht_w_assessment)
+    //     console.log("in getAss without assessment", this.state.vht_array)
+    //     console.log("in getAss", found)
+    //     return found
+    // }
 
-            if (!intersect.includes(data[itr].cvsa_id)) {
-                found.push(itr)
-            }
+
+    handleChange(event){
+        if(event.target.name == "vht_A_id")
+        {
+            this.setState({from_vht:event.target.value})
+            //(this.setState({[event.target.name]: event.target.value}))
         }
-        console.log("in getAss", intersect)
-        this.setState({vht_w_assessment: intersect})
-        console.log("in getAss with assessment", this.state.vht_w_assessment)
-        console.log("in getAss without assessment", this.state.vht_array)
-        console.log("in getAss", found)
-        return found
     }
 
+
+
     render() {
+
+        //TO DO PUT THESE INTO FUNCTIONS AND CHANGE ON SELECT CHANGE
         let vht_select_option = this.state.vht_array.map(item => <option id={item.id}
                                                                          value={item.id}> {item.id} </option>)
 
         let vht_select_option2 = this.state.vht_w_assessment.map(item => <option
                                                                                  value={item}> {item} </option>)
 
-
+        console.log("WE ARE HERE",this.state.from_vht)
         return (
-            <div className="landing-form">
-                <h1 style={{color: "white"}}>Transfer Patients</h1>
-                <label style = {{color:"white"}} >Transfer From: </label>
-                <select
-                    //value={this.state.vht_id}
-                    onChange={this.handleChange}
-                    name="vht_A_id"
-                >
-                    <option value="EMPTY"> --SELECT ONE--</option>
-                    {vht_select_option2}
-                </select>
-                <span style =  {{color:"white",paddingLeft:50}}> >>> </span>
-                <label style = {{color:"white",paddingLeft: 50}} >Transfer To: </label>
-                <select
-                    //value={this.state.vht_id}
-                    onChange={this.handleChange}
-                    name="vht_B_id"
-                >
-                    <option value="EMPTY"> --SELECT ONE--</option>
-                    {vht_select_option}
-                </select>
-                <div>
+            <div>
+                <div className = "landing-form">
+                    <h1 style={{color: "white"}}>Transfer Patients</h1>
+                    <label style = {{color:"white"}} >Transfer From: </label>
+                    <select
+                        //value={this.state.vht_id}
+                        onChange={this.handleChange}
+                        name="vht_A_id"
+                        value={this.state.vht_id}
+                    >
+                        <option value="EMPTY"> --SELECT ONE--</option>
+                        {vht_select_option2}
+                    </select>
+                    <span className = "select-style" > >>> </span>
+                    <label className = "select-style" >Transfer To: </label>
+                    <select
+                        //value={this.state.vht_id}
+                        onChange={this.handleChange}
+                        name="vht_B_id"
+                    >
+                        <option value="EMPTY"> --SELECT ONE--</option>
+                        {/*{vht_select_option}*/}
+
+                    </select>
+                </div>
+                <div className="table-position">
                 <MaterialTable
                     title="table"
                     columns={this.state.columns}
                     data={this.state.data}/>
+                </div>
+                <div className="table-position">
+                    <MaterialTable
+                        title="table"
+                        columns={this.state.columns}
+                        data={this.state.data}/>
                 </div>
             </div>
 
